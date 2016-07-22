661f60a4592ee13a0fb7e0ff45954f1c3d902cba
郭彬 <444350252@qq.com>
2016-07-22 16:04:11 +0800

    Initial Commit

---
+++ Runtime(字典转模型)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runtime(字典转模型)
+//
+//  Created by 郭彬 on 16/7/22.
+//  Copyright © 2016年 walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
